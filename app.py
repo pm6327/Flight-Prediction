59fb8af46bd5d61686446fe3f31df8fb591eb1e9
Prakhar Maheshwari <prakharmaheshwari33@gmail.com>
2025-07-24 21:55:49 +0530

    updated read

--- app.py
+++ app.py
@@ -259,14 +259,12 @@ preprocessor = Pipeline(steps=[
 	("selector", selector)
 ])
 # read the training data
-# dir_path = r"C:\Users\91830\Desktop\Fligt Sagemaker\Data\train.csv"
-# train = pd.read_csv(os.path.join(dir_path , "train.csv"))
-# X_train = train.drop(columns="price")
-# y_train = train.price.copy()
-
-train = pd.read_csv("train.csv")
+dir_path = r"C:\Users\91830\Desktop\Fligt Sagemaker\Data\train.csv"
+train = pd.read_csv(os.path.join(dir_path , "train.csv"))
 X_train = train.drop(columns="price")
 y_train = train.price.copy()
+
+
 
 # fit and save the preprocessor
 preprocessor.fit(X_train, y_train)
